0480d2e812ddf51ad172b6943b51ae4a8b995c52
asharaam1 <lohanaasharaam1@gmail.com>
2025-09-19 17:12:26 +0500

    module-1 done

--- Coursera/code.py
+++ Coursera/code.py
@@ -30,4 +30,39 @@ for i in range(10):
 print("Hello, how are you?")
 # in Bash:  echo Hello, how are you?
 
-#it' a general purpose scripting language
+# it's a general purpose scripting language.
+
+# FUNCTIONS? pieces of code that perform a unit of work e.g: print()
+# KEYWORDS? Reserved words that are used to contruct instructions e.g: if, while, for
+
+# CALCULATIONS
+print(((1 + 2) * 3) / 4)
+
+"""
+x + y
+x - y
+x * y
+x / y
+x**y
+x**2
+x**3
+x ** (1 / 2)
+x // y
+x % y
+"""
+
+print(9 ** (1 / 2))
+
+# Code Editor Vs IDEs ?
+# Code editors =>  provide features, including syntax highlighting, automatic indentation, error checking, and auto-completion. define the code structure and function.
+# IDE =>  IDE is a software application that provides comprehensive facilities for software development
+
+
+# sum.py using Python IDLE
+
+# pip install jupyterlab
+# pip install notebook
+# pip install colab
+
+
+print((9-3)/(2*(1+2)))
---
+++ Coursera/sum.py
@@ -0,0 +1,10 @@
+# Python IDLE (Code editor)
+# This program adds two numbers
+num1 = 1
+num2 = 2
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print("The sum of the numbers is:", sum)
