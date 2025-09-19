d8424c1eaceb11c48a437b522b80f5a43504b1f0
asharaam1 <lohanaasharaam1@gmail.com>
2025-09-19 12:26:14 +0500

    Chapter 1 done

---
+++ Chapter-1/module.py
@@ -0,0 +1,17 @@
+import pyjokes
+
+print("here is a joke...")
+# The under lines print random jokes
+# This is single line comment
+joke = pyjokes.get_joke()
+print(joke)
+
+
+"""
+so thanks
+that was my program 
+you are so cute
+but i hate u
+buz u are not a human
+that is multiline commit
+"""
