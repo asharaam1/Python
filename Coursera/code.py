d48709fc52fcf2a37ff12a6c470ab15704a16ee7
asharaam1 <lohanaasharaam1@gmail.com>
2025-09-19 16:04:28 +0500

    also learning from coursera to get certificate

---
+++ Coursera/code.py
@@ -0,0 +1,33 @@
+friends = ["Taylor", "Alex", "Pat", "Eli"]
+for friend in friends:
+    print("Hi " + friend)
+
+# Read official docs
+# https://docs.python.org/3/
+
+# Search answers
+# https://stackoverflow.com/
+
+
+"""
+BASH:
+for iin {1..10}; do 
+    echo Hello, World!
+done
+"""
+
+"""
+POWER SHELL:
+for ($i=1; $i-le 10; $i++){
+    Write-Host "Hello, World!"
+}
+"""
+
+for i in range(10):
+    print("Hello, World!")
+
+# compare
+print("Hello, how are you?")
+# in Bash:  echo Hello, how are you?
+
+#it' a general purpose scripting language
